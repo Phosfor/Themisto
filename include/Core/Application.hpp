4a455183e1b7045d1f792899c3929034cae16e52
Max Tyslenko <Ockonal@gmail.com>
2010-09-27 19:34:51 +0300

    Looking at fullscreen state before window creating.

--- include/Core/Application.hpp
+++ include/Core/Application.hpp
@@ -23,10 +23,9 @@ class GameApplication
         {
             short width = configManager.getValue<int>("window.width", 640);
             short height = configManager.getValue<int>("window.height", 480);
+            bool fullscreen = configManager.getValue<bool>("window.fullscreen", false);
 
-            /*TODO: Check fullscreen from config */
-
-            CL_DisplayWindow window("Themisto", width, height);
+            CL_DisplayWindow window("Themisto", width, height, fullscreen);
             LOG("The window has been created!");
 
             CL_GraphicContext gc = window.get_gc();
@@ -35,8 +34,8 @@ class GameApplication
             CL_Font_System::register_font("media/pneumatics.ttf", "Pneumatics");
             CL_Font font(gc, "Pneumatics", 60);
 
-            int currentFps, startTime, frames;
-            currentFps = startTime = frames = 0;
+            int fps, startTime, frames;
+            fps = startTime = frames = 0;
 
             while (!keyboard.get_keycode(CL_KEY_ESCAPE))
             {
@@ -49,7 +48,7 @@ class GameApplication
                     int delta = currentTime - startTime;
                     if (delta < 0 || delta > 2000)
                     {
-                        if (delta > 0) currentFps = (frames*1000) / delta;
+                        if (delta > 0) fps = (frames*1000) / delta;
                         frames = 0;
                         startTime = currentTime;
                     }
@@ -57,11 +56,17 @@ class GameApplication
                 // FPS Counter stuff ENDS here
 
                 gc.clear(CL_Colorf::gray);
-
-                cout << "FPS: " << currentFps << "\n";
-                font.draw_text(gc, 10, 70, CL_String(cl_format("fps: %1", currentFps)), CL_Colorf::black);
+                font.draw_text(gc, 10, 70, CL_String(cl_format("fps: %1", fps)), CL_Colorf::black);
 
                 CL_KeepAlive::process();
+                /*TODO: wtf! (check vsync) */
+                /*
+                 * CL_Display::flip()   - Locks to the default frame rate
+                 * CL_Display::flip(-1) - Locks to the default frame rate
+                 * CL_Display::flip(0)  - Do not lock to the frame rate
+                 * CL_Display::flip(1)  - Sync to every frame
+                 * CL_Display::flip(2)  - Sync to every 2nd frame
+                */
                 window.flip(0);
                 CL_System::sleep(10);
             }
