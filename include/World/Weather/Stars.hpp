21dc3fda4559884502bf174061d97436029b50b6
Max Tyslenko <Ockonal@gmail.com>
2010-10-16 18:17:55 +0300

    Stars now do full-cycle day.

--- include/World/Weather/Stars.hpp
+++ include/World/Weather/Stars.hpp
@@ -17,17 +17,31 @@ class Stars
         int mMaxStars;
         int mWidth, mHeight;
         float mBloomSize;
-        float t;
+        float t1, t2;
+        bool mNight;
+        bool mDrawStars;
+
+        CL_Sprite mWayPattern[3]; // Milkway texture patterns
+        vector<CL_Sprite> mMilkWay;
+        CL_Sprite mSkyPattern;
+        vector<int> x_,y_;
 
         CL_GraphicContext mGC;
         CL_BlendMode blend_mode;
+
         CL_ProgramObject mProgramBloom;
+        CL_ProgramObject mProgramBlur;
 
         CL_FrameBuffer mStarsBuf;
         CL_Texture mStarsTexture;
 
+        CL_FrameBuffer mBlurBuf;
+        CL_Texture mBlurTexture;
+
         void makeBloomHandle();
-        void renderStars(CL_ProgramObject &program);
+        void makeBlurHandle();
+
+        void renderStars(CL_ProgramObject &program, bool bloom = false);
 
     public:
         Stars(int maxStars = 200);
--- src/World/Weather/Stars.cpp
+++ src/World/Weather/Stars.cpp
@@ -31,13 +31,45 @@ Stars::Stars(int maxStars)
         color[i] = CL_Colorf(1.0f, g, b, brightness);
     }
 
+    mWayPattern[0] = CL_Sprite(mGC, "media/milkway/pattern0.png");
+    mWayPattern[1] = CL_Sprite(mGC, "media/milkway/pattern1.png");
+    mWayPattern[2] = CL_Sprite(mGC, "media/milkway/pattern2.png");
+    // Drawing the milkway
+    // y = k*x + b
+    // k = tg(L)
+    // b: y in the intersection with Y
+    for (float x=0; x < mWidth/100; x++)
+    {
+        mMilkWay.push_back(CL_Sprite(mWayPattern[rand()%3]));
+        mMilkWay[x].set_alpha(0.5);
+        float k = (rand()%15+10)/10.0f;
+        mMilkWay[x].set_scale(k, k);
+    }
+
+    mSkyPattern = CL_Sprite(mGC, "media/sky_pattern.png");
+    for (int i=0; i < 100; i++)
+    {
+        float x = rand() % mWidth;
+        float y = rand() % mHeight;
+        x_.push_back(x);
+        y_.push_back(y);
+    }
+
     mStarsTexture = CL_Texture(mGC, mGC.get_width(), mGC.get_height());
     mStarsBuf = CL_FrameBuffer(mGC);
     mStarsBuf.attach_color_buffer(0, mStarsTexture);
 
+    mBlurTexture = CL_Texture(mGC, mGC.get_width(), mGC.get_height());
+    mBlurBuf = CL_FrameBuffer(mGC);
+    mBlurBuf.attach_color_buffer(0, mBlurTexture);
+
     makeBloomHandle();
+    makeBlurHandle();
+
     mBloomSize = 0.09f;
-    t = 0.0f;
+    t1 = t2 = 0.0f;
+    mNight = false;
+    mDrawStars = false;
 
     blend_mode.set_blend_function(
             cl_blend_dest_color, cl_blend_one,
@@ -58,6 +90,33 @@ void Stars::setStarsLimit(int maxStars)
 
 void Stars::update(float hours)
 {
+    if (hours <= 10.0f) {
+        t1 = 0.0f;
+    } else if (hours >= 13.0f) {
+        t1 = 1.0f;
+        mNight = true;
+    } else {
+        mDrawStars = true;
+        t1 = ((hours - 10.0f) / (13.0f - 10.0f))/5;
+        mBloomSize = t1;
+    }
+
+    if (mNight)
+    {
+        if (hours <= 21.5f) {
+            t2 = 0.0f;
+        } else if (hours >= 23.8f) {
+            t2 = 1.0f;
+            mNight = false;
+            mDrawStars = false;
+        } else {
+            t2 = ((hours - 10.0f) / (13.0f - 10.0f))/5;
+            mBloomSize = 1.0f - t2;
+        }
+    }
+
+    if (!mDrawStars) return;
+
     // Draw all the stars to the buffer
     mGC.set_frame_buffer(mStarsBuf);
     mGC.clear(CL_Colorf::transparent);
@@ -73,19 +132,36 @@ void Stars::update(float hours)
     // Process texture with stars using bloom shader
     mGC.set_blend_mode(blend_mode);
     mGC.set_texture(0, mStarsTexture);
-    renderStars(mProgramBloom);
+    renderStars(mProgramBloom, true);
+    mGC.reset_texture(0);
+    mGC.reset_frame_buffer();
+    mGC.reset_blend_mode();
+
+    mGC.set_frame_buffer(mBlurBuf);
+    mGC.clear(CL_Colorf::transparent);
+    //mWayPattern.draw(mGC, 500, 500);
+    mGC.reset_frame_buffer();
+
+    mGC.set_blend_mode(blend_mode);
+    mGC.set_texture(0, mBlurTexture);
+    renderStars(mProgramBlur);
     mGC.reset_texture(0);
     mGC.reset_blend_mode();
 
-    if (hours <= 10.0f) {
-        t = 0.0f;
-    } else if (hours >= 13.0f) {
-        t = 1.0f;
-    } else {
-        t = ((hours - 10.0f) / (13.0f - 10.0f))/5;
-        //mBloomSize = t * 0.21;
-        mBloomSize = t;
-    }
+    /*for (float x=0; x < mWidth/100; x++)
+    {
+        float k = tan(Deg2Rad(140));
+        float x_ = x * 100;
+        float b = mHeight - mHeight*0.4;
+        float y = k*x_ + b;
+        mMilkWay[x].draw(mGC, x_, y);
+    }*/
+
+    /*mSkyPattern.set_color(CL_Colorf(41/255.0f, 87/255.0f, 102/255.0f, 0.1f));
+    for (int i=0; i < 100; i++)
+    {
+        mSkyPattern.draw(mGC, x_[i], y_[i]);
+    }*/
 }
 
 void Stars::makeBloomHandle()
@@ -101,7 +177,20 @@ void Stars::makeBloomHandle()
         throw CL_Exception("Unable to link bloom shader!");
 }
 
-void Stars::renderStars(CL_ProgramObject &program)
+void Stars::makeBlurHandle()
+{
+    mProgramBlur = CL_ProgramObject::load(mGC,
+                "media/shaders/blur/vertext_horizontal.glsl",
+                "media/shaders/blur/fragment_horizontal.glsl");
+
+    mProgramBlur.bind_attribute_location(0, "Position");
+    mProgramBlur.bind_attribute_location(1, "TextCoord0");
+
+    if (!mProgramBlur.link())
+        throw CL_Exception("Unable to link blur shader!");
+}
+
+void Stars::renderStars(CL_ProgramObject &program, bool bloom)
 {
     CL_Rectf rect(0.0f, 0.0f, (float)mGC.get_width(), (float)mGC.get_height());
     CL_Rectf texture_unit1_coords(0.0f, 0.0f, 1.0f, 1.0f);
@@ -130,7 +219,7 @@ void Stars::renderStars(CL_ProgramObject &program)
     primarray.set_attributes(0, positions);
     primarray.set_attributes(1, tex1_coords);
 
-    mProgramBloom.set_uniform1f("BloomSize", mBloomSize);
+    if (bloom) mProgramBloom.set_uniform1f("BloomSize", mBloomSize);
 
     mGC.set_program_object(program, cl_program_matrix_modelview_projection);
     mGC.draw_primitives(cl_triangles, 6, primarray);
