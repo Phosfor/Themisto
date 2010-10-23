5d7c8c6b1bdb6857e7313d58c4be37f238d3f484
Max Tyslenko <Ockonal@gmail.com>
2010-10-23 18:40:59 +0300

    Finished code of DebugDraw class. All methods are inherited and created.

--- src/Physic/DebugDraw.cpp
+++ src/Physic/DebugDraw.cpp
@@ -81,8 +81,25 @@ void DebugDraw::DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Ve
     CL_Draw::line(mGC, center.x, center.y, p.x, p.y, cl_color);
 }
 
-void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) { }
-void DebugDraw::DrawTransform(const b2Transform& xf) { }
+void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) 
+{
+    CL_Colorf cl_color(color.r, color.g, color.b);
+    CL_Draw::line(mGC, p1.x, p1.y, p2.x, p2.y, cl_color);
+}
+
+void DebugDraw::DrawTransform(const b2Transform& xf) 
+{
+    b2Vec2 p1 = xf.position, p2;
+    const float32 k_axisScale = 0.4f;
+
+    CL_Colorf color = CL_Colorf(1.0f, 0.0f, 0.0f);
+    p2 = p1 + k_axisScale * xf.R.col1;
+    CL_Draw::line(mGC, p1.x, p1.y, p2.x, p2.y, color);
+
+    CL_Colorf color2 = CL_Colorf(0.0f, 1.0f, 0.0f);
+    p2 = p1 + k_axisScale * xf.R.col2;
+    CL_Draw::line(mGC, p1.x, p1.y, p2.x, p2.y, color2);
+}
 
 void DebugDraw::DrawPoint(const b2Vec2& p, float32 size, const b2Color& color)
 {
@@ -91,4 +108,20 @@ void DebugDraw::DrawPoint(const b2Vec2& p, float32 size, const b2Color& color)
     CL_Draw::point(mGC, p.x, p.y, cl_color);
 }
 
-void DebugDraw::DrawAABB(b2AABB* aabb, const b2Color& color) { }
+void DebugDraw::DrawAABB(b2AABB* aabb, const b2Color& color)
+{
+    CL_Colorf cl_color(color.r, color.g, color.b);
+
+    CL_Draw::line(mGC, 
+            aabb->lowerBound.x, aabb->lowerBound.y,
+            aabb->upperBound.x, aabb->upperBound.y,
+            cl_color);
+    CL_Draw::line(mGC, 
+            aabb->upperBound.x, aabb->upperBound.y,
+            aabb->upperBound.x, aabb->upperBound.y,
+            cl_color);
+    CL_Draw::line(mGC, 
+            aabb->upperBound.x, aabb->upperBound.y,
+            aabb->lowerBound.x, aabb->lowerBound.y,
+            cl_color);
+}
