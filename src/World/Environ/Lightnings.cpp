717ff5af26f8884c48e7c3753fa1e5cbed1036f4
Max Tyslenko <Ockonal@gmail.com>
2010-10-25 16:12:01 +0300

    Added new lightnings surfaces. Loading them in Lightning code.

--- src/World/Environ/Lightnings.cpp
+++ src/World/Environ/Lightnings.cpp
@@ -2,10 +2,14 @@
 #include "Core/EnvironManager.hpp"
 
 Lightnings::Lightnings()
-    : EnvironObject(), mProbability(500)
+    : EnvironObject(), mProbability(200), alpha(0)
 {
     mGC = appManager.getGraphic();
-    mLightningImage = CL_Image(mGC, "media/lightnings/1.png");
+
+    for (int i=0; i < 11; ++i)
+    {
+        mLightningImage[i] = CL_Image(mGC, cl_format("media/lightnings/%1.png", i));
+    }
 
     if (!environManager.getTypeEnabled(Environ_Clouds)) 
         mEnabled = false;
@@ -18,13 +22,17 @@ void Lightnings::update(float windPower, float elapsed, float globalTime)
     if (mEnabled && !mAnimation && rand() % mProbability == 0)
     {
         mPosition = mCloudsHandle->getCloudPos();
-        if (mPosition.x != -1 && mPosition.y != -1) mAnimation = true;
+        if (mPosition.x != -1 && mPosition.y != -1)
+        {
+            mAnimation = true;
+            mHandle = rand() % 11;
+        }
     }
 
-    if (mAnimation && mTimer <= 500.0f)
+    if (mAnimation && mTimer <= 700.0f)
     {
         mTimer += elapsed * 1000.0f;
-        mLightningImage.draw(mGC, mPosition.x, mPosition.y);
+        mLightningImage[mHandle].draw(mGC, mPosition.x, mPosition.y);
     }
     else
     {
