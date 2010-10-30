d9c94ee0f07a1a9a2dec8ceef8f434c007057f13
Max Tyslenko <Ockonal@gmail.com>
2010-10-30 08:43:36 +0300

    Updated Object interface. Now there are stored a name of the object.

--- include/World/Objects/Object.hpp
+++ include/World/Objects/Object.hpp
@@ -5,8 +5,11 @@ class Object
 {
     private:
         float x, y;
+        std::string mName;
 
     public:
+        virtual std::string getName() { return mName; }
+        virtual void setName(const std::string &name) { mName = name; }
         virtual void update() = 0;
 };
 
