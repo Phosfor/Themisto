d6f94b6fdf39577cc75ead022011f77aa2f0514e
Max Tyslenko <Ockonal@gmail.com>
2011-03-18 16:21:31 +0200

    Forgotten python test file.

---
+++ bin/test.py
@@ -0,0 +1,8 @@
+import Core.LogManager;
+import Core.ConfigManager
+import Core.Utils
+
+Core.LogManager.LOG('fucking test!')
+print Core.Utils.Pi
+print Core.Utils.GetTemplateFolder()
+print Core.ConfigManager.getListValue('application')
